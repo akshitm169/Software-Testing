caf2e1c693263b19674566f9ded29d90910cd764
akshitm169 <54849887+akshitm169@users.noreply.github.com>
2022-04-11 23:26:27 +0530

    Add files via upload

---
+++ Assignment 4/P4/P4_1.py
@@ -0,0 +1,25 @@
+'''Author- Akshit Monga'''
+# Selecting from a dropdown
+
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome(executable_path="/Users/akshitmonga/WebDrivers/chromedriver")
+
+driver.get("https://www.tutorialspoint.com/selenium/selenium_automation_practice.htm")
+
+# identify dropdown with Select class
+sel = Select(driver.find_element(by=By.XPATH, value="//select[@name='continents']"))
+time.sleep(2)
+
+# select from dropdown by select_by_visible_text() method
+sel.select_by_visible_text("South America")
+time.sleep(3)
+
+# select from dropdown by select_by_index() method
+sel.select_by_index(0)
+time.sleep(3)
+
+driver.close()
